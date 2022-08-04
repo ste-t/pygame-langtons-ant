92a160cfe180fe2e5c46e8f4e295b658f42a918d
IlmastroStefanuzzo <ilmastrostefanuzzo@gmail.com>
2022-08-04 02:04:05 +0200

    Lots of optimization

--- main.py
+++ main.py
@@ -3,26 +3,25 @@ import numpy as np
 import pygame
 import pygame.gfxdraw
 
+color0 = 0x282d3f
+color1 = 0xd3423e
 
-pygame.init()
 pygame.display.init()
-screen_size = 1080, 720
-screen = pygame.display.set_mode(screen_size)
-fps = 240
+
+max_fps = 0  # 0 means no limit
 clock = pygame.time.Clock()
 
-black = 0
-color_a = 0x282d3f
-color_b = 0xd3423e
+window_size = 1080, 720
+screen = pygame.display.set_mode(window_size)
+square_size = 10  # Should be a divisor of the vertical and horizontal window size
 
-square_size = 10
-
-rows, cols = int(screen_size[1] / square_size), int(screen_size[0] / square_size)
+cols, rows = int(window_size[0] /
+                 square_size), int(window_size[1] / square_size)
 
 grid = np.zeros((rows, cols), dtype=np.uint8)
 print(f"\n{grid}\n")
 print(
-    f"Screen size: {screen_size[0]}x{screen_size[1]}\n"
+    f"Window size: {window_size[0]}x{window_size[1]}\n"
     f"Square size: {square_size}\n"
     f"Grid size: {grid.shape[1]}x{grid.shape[0]}")
 
@@ -34,7 +33,8 @@ class Ant:
     ANT_LEFT = 3
 
     def __init__(self, position, direction: int):
-        self.position: list[int] = [int(position[0]), int(position[1])]
+        # Make sure it is a list[int] and not a list[float]
+        self.position = list(map(int, position))
         self.direction = direction
 
     def turn_right(self):
@@ -45,14 +45,10 @@ class Ant:
         self.direction -= 1
         self.direction %= 4
 
-    def on_zero(self):
-        # print("Checking square color")
-        # print(not grid[self.position[1]][self.position[0]])
-        return not grid[self.position[1], self.position[0]]
-
     def switch_color(self):
         # print("Switching color")
-        grid[self.position[1], self.position[0]] = not grid[self.position[1], self.position[0]]
+        grid[self.position[1], self.position[0]
+             ] = not grid[self.position[1], self.position[0]]
 
     def move(self):
         if self.direction == self.ANT_UP:
@@ -75,50 +71,36 @@ class Ant:
             self.position[1] = rows - 1
 
     def step(self):
-        if self.on_zero():
-            # print("Left")
-            self.turn_right()
-            self.switch_color()
-            self.move()
-        else:
+        # If is on 1
+        if grid[self.position[1], self.position[0]]:
             # print("Right")
             self.turn_left()
-            self.switch_color()
-            self.move()
+        else:
+            # print("Left")
+            self.turn_right()
+        self.switch_color()
+        self.move()
 
 
-"""
-def display(surface):
-    display_surf = pygame.Surface(grid.shape)
-    display_grid = (grid*color_b) + (1-grid)*color_a
-    pygame.surfarray.blit_array(display_surf, display_grid)
-    scaled_surf = pygame.transform.scale(display_surf, surface.get_size())
+def draw_cells():
+    display_surf = pygame.Surface((grid.shape[1], grid.shape[0]))
+    pygame.surfarray.blit_array(
+        display_surf, np.transpose(grid * color1 + (1-grid)*color0))
 
-    surface.blit(scaled_surf, (0, 0))
-"""
+    # TODO: figure out why this displays the wrong color
+    #display_surf = pygame.surfarray.make_surface(np.transpose(grid * color1))
 
-
-def display(surface):
-    x, y = 0, 0
-    for row in grid:
-        for col in row:
-            if col:
-                pygame.draw.rect(surface, color_b, (x, y, square_size, square_size))
-            x += square_size
-        y += square_size
-        x = 0
+    screen.blit(pygame.transform.scale(display_surf, window_size), (0, 0))
 
 
-def draw_grid():
-    for x in range(0, screen_size[0], square_size):
-        pygame.gfxdraw.vline(screen, x, 0, screen_size[1], (0, 0, 0))
-    for y in range(0, screen_size[1], square_size):
-        pygame.gfxdraw.hline(screen, 0, screen_size[0], y, (0, 0, 0))
+grid_lines = pygame.Surface(window_size, pygame.SRCALPHA)
+for x in range(0, window_size[0], square_size):
+    pygame.gfxdraw.vline(grid_lines, x, 0, window_size[1], (0, 0, 0))
+for y in range(0, window_size[1], square_size):
+    pygame.gfxdraw.hline(grid_lines, 0, window_size[0], y, (0, 0, 0))
 
 
-ant = Ant((cols / 2, rows / 2), 1)
-
-
+ant = Ant([cols / 2, rows / 2], 1)
 # ant1 = Ant((cols / 2.5, rows / 2), 2)
 # ant2 = Ant((cols / 1.8, rows / 2), 3)
 # ant3 = Ant((cols / 3, rows / 2), 4)
@@ -126,9 +108,8 @@ ant = Ant((cols / 2, rows / 2), 1)
 
 def main():
     steps = 0
-
     while True:
-        clock.tick(fps)
+        clock.tick(max_fps)
 
         # Exit on ESCAPE key press or if the user presses the X button on the window
         for event in pygame.event.get():
@@ -136,22 +117,23 @@ def main():
                     event.type == pygame.KEYDOWN and \
                     event.key == pygame.K_ESCAPE:
                 print(steps)
-                sys.exit()
+                sys.exit(0)
 
         ant.step()
         # ant1.step()
         # ant2.step()
         # ant3.step()
 
-        screen.fill(color_a)
-        display(screen)
-        draw_grid()
+        draw_cells()
+        screen.blit(grid_lines, (0, 0))
         pygame.display.flip()
-        pygame.display.set_caption(f"Langton's ant ({round(clock.get_fps())}/{fps} fps)")
-        steps += 1
+
+        pygame.display.set_caption(
+            f"Langton's ant ({int(clock.get_fps())}/{max_fps} fps)")
 
         """ Performance testing
-        if steps >= 100:
+        steps += 1
+        if steps >= 11000:
             print(f"\n{steps} steps in {pygame.time.get_ticks()} ms")
             sys.exit()
         """
